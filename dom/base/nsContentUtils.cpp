01b75db689f19c9306e693c597b82556522245d8
Cameron Kaiser <classilla@floodgap.com>
2019-08-17 10:42:19 -0700

    #565: M1358297 parts 3 and 4
    (cherry picked from commit 3fd15a87ada28cd7eb9b23d9d2f55999e65cd5f8)

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -6398,9 +6398,7 @@ nsContentUtils::FlushLayoutForTree(nsIDOMWindow* aWindow)
 
 void nsContentUtils::RemoveNewlines(nsString &aString)
 {
-  // strip CR/LF and null
-  static const char badChars[] = {'\r', '\n', 0};
-  aString.StripChars(badChars);
+  aString.StripCRLF();
 }
 
 void
