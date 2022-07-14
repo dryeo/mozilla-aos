bed9571e1acbbf495345708453ee035e1e3575df
Dave Yeo <dave.r.yeo@gmail.com>
2022-07-13 22:33:22 -0700

    Fix a missing symbol when using GCC 11.x

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -2487,6 +2487,16 @@ gfxFont::GetShapedWord(gfxContext *aContext,
     return sw;
 }
 
+template gfxShapedWord* gfxFont::GetShapedWord(gfxContext *aContext,
+                                               const unsigned char    *aText,
+                                               uint32_t    aLength,
+                                               uint32_t    aHash,
+                                               int32_t      aRunScript,
+                                               bool        aVertical,
+                                               int32_t     aAppUnitsPerDevUnit,
+                                               uint32_t    aFlags,
+                                               gfxTextPerfMetrics *aTextPerf GFX_MAYBE_UNUSED);
+
 bool
 gfxFont::CacheHashEntry::KeyEquals(const KeyTypePointer aKey) const
 {
